058ef1c1b41ece29fa2764a04c03213f3a502ef6
Lucas Ribeiro <lucas.l.rg@hotmail.com>
2022-01-11 20:23:34 -0300

    footer e header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import FooterComponent from './component/FooterComponent';
+import HeaderComponent from './component/HeaderComponent';
 import ListEmployeeComponent from './component/ListEmployeeComponent';
 
 
 function App() {
   return (
     <div>
-      <ListEmployeeComponent/>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<ListEmployeeComponent/>} />
+            <Route path="/employeers" element={<ListEmployeeComponent/>}/>
+         </Routes> 
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
